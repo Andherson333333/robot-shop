02a5ca569478859e4813dc15385537f1df578d6c
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-03-12 08:29:46 -0400

    Update nginx-helm-internal.tf

--- Infrastructure-cloud-EKS/Loadbalancer-aws-nginx/nginx-helm-internal.tf
+++ Infrastructure-cloud-EKS/Loadbalancer-aws-nginx/nginx-helm-internal.tf
@@ -1,88 +1,109 @@
 # Helm release for internal nginx ingress controller
 resource "helm_release" "internal-nginx" {
- # Basic chart configuration
- name             = "internal"
- repository       = "https://kubernetes.github.io/ingress-nginx"
- chart            = "ingress-nginx"
- namespace        = "ingress-nginx-internal"
- create_namespace = true
- version          = "4.12.0"
+  # Basic chart configuration
+  name             = "internal"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx-internal"
+  create_namespace = true
+  version          = "4.12.0"
 
- # Ingress class configuration
- set {
-   name  = "controller.ingressClassResource.name"
-   value = "nginx-internal"
- }
- set {
-   name  = "controller.ingressClassResource.controllerValue" 
-   value = "k8s.io/ingress-nginx-internal"
- }
- set {
-   name  = "controller.watchIngressWithoutClass"
-   value = "false"
- }
+  # Configuración para instalarse en nodos de infraestructura
+  set {
+    name  = "controller.nodeSelector.node-type"
+    value = "infrastructure"
+  }
 
- # AWS Load Balancer configuration
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-internal"
-   value = "true"
- }
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-   value = "nlb"
- }
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
-   value = "ip"
- }
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
-   value = "arn:aws:acm:us-east-1:XXXXXXXXXXXX:certificate/XXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX"
- }
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-deletion-protection"
-   value = "false"
- }
+  set {
+    name  = "controller.nodeSelector.workload-type"
+    value = "platform"
+  }
 
- # Port configuration
- # set {
- #   name  = "controller.service.ports.http"
- #   value = "80"
- # }
- # set {
- #   name  = "controller.service.enableHttp"
- #   value = "true"
- # }
- set {
-   name  = "controller.service.ports.https"
-   value = "443"
- }
+  # Tolerations para el taint de infraestructura
+  set {
+    name  = "controller.tolerations[0].key"
+    value = "workload-type"
+  }
 
- # Explicitly disable HTTP
- set {
-   name  = "controller.service.enableHttp"
-   value = "false"
- }
+  set {
+    name  = "controller.tolerations[0].value"
+    value = "infrastructure"
+  }
 
- # SSL/TLS configuration
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
-   value = "443"
- }
- set {
-   name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
-   value = "http"
- }
- set {
-   name  = "controller.service.targetPorts.https"
-   value = "http"
- }
+  set {
+    name  = "controller.tolerations[0].effect"
+    value = "PreferNoSchedule"
+  }
 
- # Additional configurations
- set {
-   name  = "controller.config.use-forwarded-headers"
-   value = "true"
- }
-
- depends_on = [helm_release.aws_lbc]
+  # Ingress class configuration
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "nginx-internal"
+  }
+  set {
+    name  = "controller.ingressClassResource.controllerValue" 
+    value = "k8s.io/ingress-nginx-internal"
+  }
+  set {
+    name  = "controller.watchIngressWithoutClass"
+    value = "false"
+  }
+  # AWS Load Balancer configuration
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-internal"
+    value = "true"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "nlb"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
+    value = "ip"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+    value = "arn:aws:acm:us-east-1:XXXXXXXXXXXX:certificate/XXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-deletion-protection"
+    value = "false"
+  }
+  # Port configuration
+  # set {
+  #   name  = "controller.service.ports.http"
+  #   value = "80"
+  # }
+  # set {
+  #   name  = "controller.service.enableHttp"
+  #   value = "true"
+  # }
+  set {
+    name  = "controller.service.ports.https"
+    value = "443"
+  }
+  # Explicitly disable HTTP
+  set {
+    name  = "controller.service.enableHttp"
+    value = "false"
+  }
+  # SSL/TLS configuration
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+    value = "443"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
+    value = "http"
+  }
+  set {
+    name  = "controller.service.targetPorts.https"
+    value = "http"
+  }
+  # Additional configurations
+  set {
+    name  = "controller.config.use-forwarded-headers"
+    value = "true"
+  }
+  depends_on = [helm_release.aws_lbc]
 }
