ad9a1c1e48803a46738576b6ab96bd1f5a038113
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-03-12 08:22:01 -0400

    Update karpenter-controler.tf

--- Infrastructure-cloud-EKS/Karpenter/karpenter-controler.tf
+++ Infrastructure-cloud-EKS/Karpenter/karpenter-controler.tf
@@ -1,18 +1,20 @@
 # Karpenter Module
 module "eks_karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "20.23.0"
+  version = "20.33.1"
 
-  cluster_name = module.eks.cluster_name
-  namespace			  = "karpenter"
+  cluster_name                    = module.eks.cluster_name
+  namespace                       = "karpenter"
   enable_pod_identity             = true
   create_pod_identity_association = true
+
+  enable_spot_termination         = true
+  enable_v1_permissions           = true
 
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
 
   tags = local.tags
-
-  depends_on = [kubectl_manifest.karpenter_namespace]
+  depends_on = [kubectl_manifest.karpenter_namespace,data.aws_iam_role.spot]
 }
