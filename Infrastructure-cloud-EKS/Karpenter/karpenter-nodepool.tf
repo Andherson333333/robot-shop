9f838e93d3eb26519462177047ea496ce0057cc0
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-03-12 08:23:15 -0400

    Create karpenter-nodepool.tf

---
+++ Infrastructure-cloud-EKS/Karpenter/karpenter-nodepool.tf
@@ -0,0 +1,23 @@
+# Karpenter Node class común para todos los NodePools
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1
+    kind: EC2NodeClass
+    metadata:
+      name: default
+    spec:
+      amiFamily: AL2023
+      role: ${module.eks_karpenter.node_iam_role_name}
+      amiSelectorTerms:
+        - alias: al2023@latest
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+  YAML
+  depends_on = [helm_release.karpenter]
+}
