f56867e79520f64f014e893e1945d7e3ea1289dd
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-03-12 08:26:36 -0400

    Update argocd-helm.tf

--- Infrastructure-cloud-EKS/Argocd-helm/argocd-helm.tf
+++ Infrastructure-cloud-EKS/Argocd-helm/argocd-helm.tf
@@ -1,39 +1,71 @@
-# Base ArgoCD Installation Block
-# Defines the main configuration for ArgoCD deployment
 resource "helm_release" "argocd" {
- name             = "argocd"
- repository       = "https://argoproj.github.io/argo-helm"
- chart            = "argo-cd" 
- namespace        = "argocd"
- version          = "7.7.13"
- create_namespace = true
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "7.7.13"
+  create_namespace = true
 
- # Storage Configuration Block
- # Sets up persistent storage settings for Redis
- set {
-   name  = "redis.enabled"
-   value = "true"
- }
- set {
-   name  = "redis.persistence.enabled" 
-   value = "true"
- }
- set {
-   name  = "redis.persistence.storageClass"
-   value = "gp3-default"
- }
+  # Configuración para desplegar en nodos de infraestructura
+  set {
+    name  = "global.nodeSelector.node-type"
+    value = "infrastructure"
+  }
 
- # ArgoCD Server Configuration Block
- # Configures core ArgoCD server settings
- set {
-   name  = "configs.params.server\\.insecure"
-   value = "true"
- }
+  set {
+    name  = "global.nodeSelector.workload-type"
+    value = "platform"
+  }
 
- # Dependencies Block
- # Defines required resources that must exist first
- depends_on = [
-   helm_release.nginx_internal,
-   helm_release.external_nginx
- ]
+  set {
+    name  = "global.tolerations[0].key"
+    value = "workload-type"
+  }
+
+  set {
+    name  = "global.tolerations[0].value"
+    value = "infrastructure"
+  }
+
+  set {
+    name  = "global.tolerations[0].effect"
+    value = "PreferNoSchedule"
+  }
+
+  # Redis configuration
+  set {
+    name  = "redis.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "redis.persistence.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "redis.persistence.storageClass"
+    value = "gp3-default"
+  }
+
+  set {
+    name  = "redis.persistence.size"
+    value = "10Gi"
+  }
+
+  set {
+    name  = "redis.persistence.accessMode"
+    value = "ReadWriteOnce"
+  }
+
+  # Server configuration
+  set {
+    name  = "configs.params.server\\.insecure"
+    value = "true"
+  }
+
+  depends_on = [
+    helm_release.external_nginx,
+    kubectl_manifest.karpenter_infra_node_pool
+  ]
 }
