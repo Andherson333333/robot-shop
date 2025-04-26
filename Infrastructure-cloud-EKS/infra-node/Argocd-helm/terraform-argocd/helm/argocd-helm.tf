cb409e712574d717dafcaaf54e695c78d61d4619
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-04-25 20:25:42 -0400

    Update argocd-helm.tf

--- Infrastructure-cloud-EKS/infra-node/Argocd-helm/terraform-argocd/helm/argocd-helm.tf
+++ Infrastructure-cloud-EKS/infra-node/Argocd-helm/terraform-argocd/helm/argocd-helm.tf
@@ -5,43 +5,35 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   version          = "7.8.13"
   create_namespace = true
-
-  # Configuración global para todos los componentes
+  # Global configuration for all components
   set {
     name  = "global.nodeSelector.node-type"
     value = "infrastructure"
   }
-
   set {
     name  = "global.nodeSelector.workload-type"
     value = "platform"
   }
-
   set {
     name  = "global.tolerations[0].key"
     value = "workload-type"
   }
-
   set {
     name  = "global.tolerations[0].value"
     value = "infrastructure"
   }
-
   set {
     name  = "global.tolerations[0].effect"
     value = "PreferNoSchedule"
   }
-
-  # Server configuration - Habilitar modo inseguro para evitar redirecciones internas
+  # Server configuration - Enable insecure mode to avoid internal redirects
   set {
     name  = "configs.params.server\\.insecure"
     value = "true"
   }
-
-  # Dependencias
+  # Dependencies
   depends_on = [
     helm_release.prometheus_stack,
     kubectl_manifest.karpenter_infra_node_pool
   ]
 }
-
