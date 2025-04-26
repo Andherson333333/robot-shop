d0a02f87a0b55be776e98ac7fed0824483521148
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-04-25 20:39:08 -0400

    Update aws-lb-helm.tf

--- Infrastructure-cloud-EKS/infra-terraform/Loadbalancer-aws-nginx/aws-lb-helm.tf
+++ Infrastructure-cloud-EKS/infra-terraform/Loadbalancer-aws-nginx/aws-lb-helm.tf
@@ -5,44 +5,36 @@ resource "helm_release" "aws_lbc" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
   version    = "1.11.0"
-
   set {
     name  = "clusterName"
     value = module.eks.cluster_name
   }
-
   set {
     name  = "serviceAccount.name"
     value = "aws-load-balancer-controller"
   }
-
-  # Configuración para instalarse en nodos de infraestructura
+  # Configuration to install on infrastructure nodes
   set {
     name  = "nodeSelector.node-type"
     value = "infrastructure"
   }
-
   set {
     name  = "nodeSelector.workload-type"
     value = "platform"
   }
-
-  # Tolerations para el taint de infraestructura
+  # Tolerations for infrastructure taint
   set {
     name  = "tolerations[0].key"
     value = "workload-type"
   }
-
   set {
     name  = "tolerations[0].value"
     value = "infrastructure"
   }
-
   set {
     name  = "tolerations[0].effect"
     value = "PreferNoSchedule"
   }
-
   depends_on = [
     module.aws_lb_controller_pod_identity
   ]
