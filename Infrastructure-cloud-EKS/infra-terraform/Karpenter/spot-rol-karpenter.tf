2165f2ed6a5ac7c5f102454fe8fcfefbd416c9fc
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-04-25 20:38:12 -0400

    Update spot-rol-karpenter.tf

--- Infrastructure-cloud-EKS/infra-terraform/Karpenter/spot-rol-karpenter.tf
+++ Infrastructure-cloud-EKS/infra-terraform/Karpenter/spot-rol-karpenter.tf
@@ -1,17 +1,15 @@
-# Este script se ejecuta una sola vez y no fallará aunque el rol ya exista
+# This script runs once and won't fail even if the role already exists
 resource "null_resource" "create_spot_role_if_not_exists" {
   provisioner "local-exec" {
     command = "aws iam get-role --role-name AWSServiceRoleForEC2Spot || aws iam create-service-linked-role --aws-service-name spot.amazonaws.com"
   }
 }
-
-# Espera un momento para que el rol se propague
+# Wait a moment for the role to propagate
 resource "time_sleep" "wait_for_role_propagation" {
   depends_on = [null_resource.create_spot_role_if_not_exists]
   create_duration = "10s"
 }
-
-# Una vez creado o verificado, podemos referenciarlo
+# Once created or verified, we can reference it
 data "aws_iam_role" "spot" {
   name = "AWSServiceRoleForEC2Spot"
   depends_on = [time_sleep.wait_for_role_propagation]
